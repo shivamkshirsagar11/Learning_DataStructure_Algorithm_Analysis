d1023284a57e697664cc6eb114a53eec6c67ca79
shivamkshirsagar11 <shivamkshirsagar2002@gmail.com>
2022-10-29 19:03:17 +0530

    i am makiing my tree utility with many options

---
+++ TreeUtil/Tree.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self, data, parent=None,left=None,right=None,root=None):
+        self.data = data
+        self.parent = parent
+        self.left = left
+        self.right = right
+        self.root = root
